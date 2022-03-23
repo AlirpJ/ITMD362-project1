2122235a9880898c26204d24980e3d1f14c19514
Joshua Prila <jprila@hawk.iit.edu>
2022-03-23 12:08:18 -0500

    Update form functionality

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,41 @@
+
+
+var html = document.querySelector('html');
+
+// Add a `js` class for any JavaScript-dependent CSS
+// See https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+html.classList.add('js');
+
+if(html.id === 'payment') {
+  formPayment = document.querySelector('form[name="payment"]');
+  restoreFormDataFromLocalStorage(formPayment.name);
+  formPayment.addEventListener('input', debounce(handleFormInputActivity, 850));
+  formPayment.addEventListener('change', handleFormInputActivity);
+  formPayment.addEventListener('submit', handleFormSubmission);
+}
+
 /*
   Core Functions
 */
+
+function handleFormInputActivity(event) {
+  var inputElements = ['INPUT', 'SELECT'];
+  var targetElement = event.target;
+  var targetType = targetElement.getAttribute('type');
+  var errorText = capitalizeFirstLetter(targetElement.name);
+  var submitButton = document.getElementById('submit');
+  var errorClass = targetElement.name + '-error';
+  var errorEl = document.querySelector('.' + errorClass);
+
+  // Regex for validating inputs
+  var ccCheck = /^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9]{2})[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35[0-9]{3})[0-9]{11}))$/;
+  var cvvCheck = /^[0-9]{3,4}$/;
+  var zipCheck = /^\d{5}(?:[-\s]\d{4})?$/;
+  var telCheck = /\d{9}/;
+
+  if (!inputElements.includes(targetElement.tagName) || targetElement.name === 'billing-address-two' || targetElement.name === 'shipping-address-two' || targetElement.name === 'billing-city' || targetElement.name === 'shipping-city') {
+    return; // this is not an element we care about
+  }
 
 function writeFormDataToLocalStorage(formName, inputElement) {
     var formData = findOrCreateLocalStorageObject(formName);
@@ -82,3 +117,9 @@ function writeFormDataToLocalStorage(formName, inputElement) {
       el.innerText = formValues[i][1];
     }
   }
+  function handleFormSubmission(event) {
+  var targetElement = event.target;
+  event.preventDefault(); // STOP the default browser behavior
+  writeFormDataToLocalStorage(targetElement.name); // STORE all the form data
+  window.location.href = targetElement.action; // PROCEED to the URL referenced by the form action
+}
